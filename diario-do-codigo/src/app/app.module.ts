bdc42afa3f78e3013faec718210288f2521edf0d
cleiton7 <cleiton.souza04@gmail.com>
2020-05-03 20:08:28 -0300

    criar componente sobre

--- diario-do-codigo/src/app/app.module.ts
+++ diario-do-codigo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
